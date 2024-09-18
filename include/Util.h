f2ef8bc0fa041fc1820836de23c88d5a67ecda27
Atri Sarkar <atrisarkar91@gmail.com>
2024-09-18 16:12:05 +0530

    Print line numbers for UNREACHABLE macro

--- include/Util.h
+++ include/Util.h
@@ -5,6 +5,10 @@
 {\
     std::cout << (message) << std::endl;\
     std::exit(-1);\
-} while(0)\
+} while(0)
 
-#define HKSL_UNREACHABLE() HKSL_ERROR("Unreachable")
+#define HKSL_UNREACHABLE() do\
+{\
+    std::cout << "Reached unreachable code in " << __FILE__ << ":" << __LINE__ << std::endl; \
+    std::exit(-1);\
+} while(0)
